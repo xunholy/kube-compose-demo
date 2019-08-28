f2103d2e519c39f66d0dd2355fa184b8980e0d22
jbrekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-08-28 19:17:35 +1000

    add comments

--- run-tests.sh
+++ run-tests.sh
@@ -3,8 +3,11 @@ set -xeuo pipefail
 GREETING=$(curl --fail --silent http://backend:8081/)
 if [[ $GREETING != 'Hello Kubernetes User Group! You were here on 29 August 2019!' ]]; then
     1>&2 echo "test failed!"
-    sleep 10
+    
+    # Sleep so kube-compose's log stream gets a chance to pick up on the above echo (kube-compose is a bit too eager when closing watch).
+    sleep 3
     exit 1
 fi
 echo "test succeeded"
-sleep 10
+# Sleep so kube-compose's log stream gets a chance to pick up on the above echo (kube-compose is a bit too eager when closing watch).
+sleep 3
